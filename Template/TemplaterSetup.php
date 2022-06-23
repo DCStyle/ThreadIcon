13404b695f133a69f68f1fd3b41bb3d33f27d13a
DCStyle <dcstylexf@gmail.com>
2022-06-23 10:53:34 +0700

    Update Icon List

--- Template/TemplaterSetup.php
+++ Template/TemplaterSetup.php
@@ -4,29 +4,16 @@ namespace DC\ThreadIcon\Template;
 
 class TemplaterSetup
 {
-    public function fnThreadTitleIcon($templater, &$escape, \DC\ThreadIcon\XF\Entity\Thread $thread, $style = 'regular', $class = 'thread-title')
+    public function fnThreadTitleIcon($templater, &$escape, \DC\ThreadIcon\XF\Entity\Thread $thread, $class = 'thread-title')
     {
         $escape = false;
 
         $icon = $thread->Icon;
         $iconTag = '';
 
-        switch($style)
-        {
-            case 'light':
-                $iconStyle = 'fal';
-                break;
-            case 'bold':
-                $iconStyle = 'fas';
-                break;
-            default:
-                $iconStyle = 'far';
-                break;
-        }
-
         if ($icon)
         {
-            $iconTag = "<i class=\"fa--xf {$iconStyle} fa-{$icon->icon}\" aria-hidden=\"true\"></i>";
+            $iconTag = "<i class=\"fa--xf {$icon->icon}\" aria-hidden=\"true\"></i>";
         }
 
         $title = "<span class=\"{$class}\" title=\"{$thread->title}\">{$thread->title}</span>";
